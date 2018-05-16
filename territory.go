8c006476448ae85c164f5dbbad59b0bca6b3ba4e
Leon Bogaert <leon@tim-online.nl>
2018-05-16 12:50:48 +0200

    Don't serialize empty Territory type

--- territory.go
+++ territory.go
@@ -1,5 +1,12 @@
 package globe
 
+import (
+	"encoding/xml"
+
+	"github.com/cydev/zero"
+	"github.com/tim-online/go-exactglobe/omitempty"
+)
+
 type Territories []Territory
 
 type Territory struct {
@@ -8,4 +15,12 @@ type Territory struct {
 
 	// Description         string   `xml:"Description,omitempty"`
 	MultiDescriptions []string `xml:"Description,omitempty"`
+}
+
+func (t Territory) MarshalXML(enc *xml.Encoder, start xml.StartElement) error {
+	return omitempty.MarshalXML(t, enc, start)
+}
+
+func (t Territory) IsEmpty() bool {
+	return zero.IsZero(t)
 }
